4836bfd12bf8d3f428ea4c2d491d9c42e559143b
James-L-Jr <james.f.logan2@gmail.com>
2024-08-28 21:37:52 -0500

    initialize project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: 'home', component: HomeComponent },
+  // Add other routes here
+  { path: '**', redirectTo: '/home' } // Wildcard route for a 404 page (optional)
+];
