a418395b3bfdb051639e601861d9e80a60796fe5
James-L-Jr <james.f.logan2@gmail.com>
2024-09-02 20:00:03 -0500

    experiment 36

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -45,7 +45,7 @@ export class ProjectsComponent {
       title: 'Plant Device Observability - 2024',
       description: 'A near real-time health and performance observability platform for manufacturing plants in the U.S. and South America.',
       imageUrl: 'grafana.png',
-      tags: ['Grafana', 'Victoria Metrics', 'PostgreSQL','VMagent', 'Kubernetes', 'Alerting'],
+      tags: ['Grafana', 'Victoria Metrics', 'PostgreSQL', 'Kubernetes', 'Alerting'],
       additionalInfo: 'Robust web-hosted observability platform tailored for manufacturing plants. ' + 
                       'Contributed to more than ten intuitive Grafana dashboards and 100 data visualizations designed to present critical data clearly to plant workers. ' +
                       'Utilized a well-tuned VictoriaMetrics configuration for efficient time-series data storage and querying, ensuring high performance and scalability. ' +
@@ -64,12 +64,14 @@ export class ProjectsComponent {
     },
     {
       title: 'Victoria Metrics Research - 2024',
-      description: 'Automated accessibility testing on a web application for a fortune 500 company.',
-      imageUrl: 'axe-core.png',
-      tags: ['Typescript', 'Jest', 'Jest-axe','Node.js', 'Dev Tools'],
-      additionalInfo: 'Jest-oriented automated testing framework for an angular application reaching testing validation goals. ' + 
-                      'Established test beds to simulate application environments and perform unit and accessibility testing. ' +
-                      'Axe-core integration via Jest-axe to verify compliance wtih WCAG standards.',
+      description: 'Upgrade Victoria Metrics to a more effective and efficient time-series database.',
+      imageUrl: 'kubernetes.png',
+      tags: ['Kubernetes', 'Victoria Metrics', 'VMagent', 'Azure pipelines', 'Microsoft Excel'],
+      additionalInfo: 'Provide a proof of concept implementation of the VictoriaMetrics cluster version as part of efforts to explore upgrades to time-series database systems. ' + 
+                      'In-depth research of best practices and implementation strategies for the deployment and cluster configuration to load balance a scalable, high-performance solution. ' +
+                      'Configurations managed by precise pipeline files and manifests, balancing VM operations for peak performance and efficient resource utilization.' +
+                      'Isolated test environment successfully demonstrating the cluster handling a data volume with no errors or loss of data. ' +
+                      'Highly detailed documentation that delineates performance contrasts between versions and provides a step-by-step implementation guide for the VM cluster solution.',
       expanded: false
     },
     {
@@ -77,10 +79,20 @@ export class ProjectsComponent {
       description: 'Adopt CI/CD automation processes for an application with an invested $50 million in support of manufacturing operations.',
       imageUrl: 'azure.png',
       tags: ['Azure DevOps', 'Azure Cloud', 'Azure App Service','Azure CLI', 'YAML', 'PowerShell'],
-      additionalInfo: 'Comprehensive single-click pipelines for Azure Cloud deployment and CI/CD tools. ' + 
+      additionalInfo: 'Comprehensive single-click pipelines for Azure Cloud deployment and CI/CD tools. ' +
                       'SonarQube, Fortify, Open Source Scanning software implemented with scheduled automation tasks. ' +
                       'Custom control over the environment used, approval workflow, and enhanced logging by environment. ' +
                       'Apply application settings from a secure vault to the Azure service simultaneously with deployment.',
+      expanded: false
+    },
+    {
+      title: 'Oracle Database Upgrade - 2023',
+      description: 'Analyst for Oracle database upgrade efforts for a major U.S. bank.',
+      imageUrl: 'jira.png',
+      tags: ['Powerpoint', 'Jira', 'Slack'],
+      additionalInfo: 'Work closely with a team of testers, developers, and business stakeholders to validate migration progress. ' + 
+                      'Maintain a regular business package that details iterative statuses in regards to project efforts. ' +
+                      'Validate results of smoke and regression test suites and determine root causes of defects.',
       expanded: false
     },
   ];
